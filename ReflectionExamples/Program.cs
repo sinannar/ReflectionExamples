7f259eac01db77b40dcc969b28fa04629b940005
Sinan NAR <sinan.nar@gmail.com>
2018-01-10 16:09:56 +1300

    Add project files.

---
+++ ReflectionExamples/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReflectionExamples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
