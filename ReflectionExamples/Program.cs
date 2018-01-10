f584270626bb963e46b8f4fc37697ab0fe083b37
Sinan NAR <sinan.nar@gmail.com>
2018-01-10 16:14:11 +1300

    what is reflection has been descriped as comment

--- ReflectionExamples/Program.cs
+++ ReflectionExamples/Program.cs
@@ -4,6 +4,18 @@ namespace ReflectionExamples
 {
     class Program
     {
+        /*
+            
+            A .Net application doesnt just contain data and code, it also contain medata which is information about the data
+            This means that an application contains the code that defines the application and data that descripes the code.
+            An attribute is one of type of metadata that can be stored in .Net application. Other types of metadata informations about the type, code , assembly and all other elements.
+            Reflection is the process of retrieving this metadata at runtime.
+
+
+
+
+
+             */
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
